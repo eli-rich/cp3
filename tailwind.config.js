f1bef36942e0c5aa7304d5efefa50ab4110c9eb3
BlazingFire007 <elijahrich02@icloud.com>
2022-10-17 18:44:11 -0500

    inital and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultFonts = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ['./src/**/*.{jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        xxs: '390px', // min-width
+      },
+      fontFamily: {
+        sans: ['Open Sans', ...defaultFonts.fontFamily.sans],
+      },
+      backgroundImage: {
+        'main-hero-bg': 'url("/img/head-prod2.webp")',
+      },
+    },
+  },
+  daisyui: {
+    themes: ['autumn'],
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+};
